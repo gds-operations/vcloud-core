c8025ad8bd7704e2b90286870da33ce24dffeb7f
Sneha Somwanshi <sneha.vishwas-somwanshi@digital.cabinet-office.gov.uk>
2014-01-29 14:26:59 +0000

    more integration tests for vcloud_gateway_interface_by_id
    Added tests for following schenarios:
    - test for getting vcloud attrs for orgVdcNetwork
    - return nil if gateway_interface with given id not found

--- spec/integration/edge_gateway/edge_gateway_spec.rb
+++ spec/integration/edge_gateway/edge_gateway_spec.rb
@@ -3,11 +3,26 @@ require 'spec_helper'
 module Vcloud
   module Core
     describe EdgeGateway do
-      it "should return provider networks for given edge gateway" do
-        edge_gateway = EdgeGateway.get_by_name(ENV['VCLOUD_EDGE_GATEWAY'])
-        gateway_interface = edge_gateway.vcloud_gateway_interface_by_id(ENV['VCLOUD_PROVIDER_NETWORK_ID'])
-        expect(gateway_interface[:Network]).not_to be_nil
-        expect(gateway_interface[:Network][:href]).to include(ENV['VCLOUD_PROVIDER_NETWORK_ID'])
+      context "get vcloud attributes for given gateway interface id " do
+        before(:all) do
+          @edge_gateway = EdgeGateway.get_by_name(ENV['VCLOUD_EDGE_GATEWAY'])
+        end
+        it "should return provider network" do
+          gateway_interface = @edge_gateway.vcloud_gateway_interface_by_id(ENV['VCLOUD_PROVIDER_NETWORK_ID'])
+          expect(gateway_interface[:Network]).not_to be_nil
+          expect(gateway_interface[:Network][:href]).to include(ENV['VCLOUD_PROVIDER_NETWORK_ID'])
+        end
+
+        it "should return orgVdcNetwork" do
+          gateway_interface = @edge_gateway.vcloud_gateway_interface_by_id(ENV['VCLOUD_NETWORK1_ID'])
+          expect(gateway_interface[:Network]).not_to be_nil
+          expect(gateway_interface[:Network][:href]).to include(ENV['VCLOUD_NETWORK1_ID'])
+        end
+
+        it "return nil if network with given id is not found" do
+          gateway_interface = @edge_gateway.vcloud_gateway_interface_by_id(SecureRandom.uuid)
+          expect(gateway_interface).to be_nil
+        end
       end
     end
   end

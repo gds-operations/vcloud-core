7bab6d896d6181ab6187a9a4312db980bb58f710
Anna Shipman <anna@annashipman.co.uk>
2014-03-10 10:30:26 +0000

    Pull out location of data directory

--- spec/vcloud/core/config_loader_spec.rb
+++ spec/vcloud/core/config_loader_spec.rb
@@ -4,31 +4,32 @@ describe Vcloud::ConfigLoader do
 
   before(:all) do
     @valid_config = valid_config
+    @data_dir = File.join(File.dirname(__FILE__), "/data")
   end
 
   it "should create a valid hash when input is JSON" do
-    input_file = 'spec/vcloud/core/data/working.json'
+    input_file = "#{@data_dir}/working.json"
     loader = Vcloud::ConfigLoader.new
     actual_config = loader.load_config(input_file)
     valid_config.should eq(actual_config)
   end
 
   it "should create a valid hash when input is YAML" do
-    input_file = 'spec/vcloud/core/data/working.yaml'
+    input_file = "#{@data_dir}/working.yaml"
     loader = Vcloud::ConfigLoader.new
     actual_config = loader.load_config(input_file)
     valid_config.should eq(actual_config)
   end
 
   it "should create a valid hash when input is YAML with anchor defaults" do
-    input_file = 'spec/vcloud/core/data/working_with_defaults.yaml'
+    input_file = "#{@data_dir}/working_with_defaults.yaml"
     loader = Vcloud::ConfigLoader.new
     actual_config = loader.load_config(input_file)
     valid_config['vapps'].should eq(actual_config['vapps'])
   end
 
 #  it "should validate correctly against a schema" do
-#    input_file = 'spec/vcloud/core/data/working_with_defaults.yaml'
+#    input_file = "#{@data_dir}/working_with_defaults.yaml"
 #    loader = Vcloud::ConfigLoader.new
 #    schema = Vcloud::Launch.new.config_schema
 #    actual_config = loader.load_config(input_file, schema)
@@ -36,7 +37,7 @@ describe Vcloud::ConfigLoader do
 #  end
 
 #  it "should raise an error if checked against an invalid schema" do
-#    input_file = 'spec/vcloud/data/working_with_defaults.yaml'
+#    input_file = "spec/vcloud/data/working_with_defaults.yaml"
 #    loader = Vcloud::ConfigLoader.new
 #    Vcloud.logger.should_receive(:fatal).with("vapps: is not a hash")
 #    expect { loader.load_config(input_file, invalid_schema) }.

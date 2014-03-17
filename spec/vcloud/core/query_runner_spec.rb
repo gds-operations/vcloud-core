6c8346199ed59b4a26ea0e5bee56820b4b4dc25b
Dan Abel <dan.abel@digital.cabinet-office.gov.uk>
2014-03-17 11:18:14 +0000

    additional tests to complete coverage of QueryRunner

--- spec/vcloud/core/query_runner_spec.rb
+++ spec/vcloud/core/query_runner_spec.rb
@@ -7,69 +7,165 @@ describe Vcloud::QueryRunner do
     @query_runner = Vcloud::QueryRunner.new()
   end
 
-  it 'should return no results when fog returns no results' do
-    @mock_fog_interface.stub(:get_execute_query).and_return({})
+  context '#available_query_types' do
+    it 'should return empty array if no data' do
+      @mock_fog_interface.stub(:get_execute_query).and_return({:Link => {}})
 
-    result = @query_runner.run()
+      result = @query_runner.available_query_types
 
-    result.should == []
+      result.size.should == 0
+    end
+
+    it 'should parse the query types returned' do
+      @mock_fog_interface.stub(:get_execute_query).and_return(
+        {:Link => [
+          {:rel  => 'down',
+           :href => 'query?type=alice&#38;format=references'},
+        ]})
+
+      result = @query_runner.available_query_types
+
+      result.size.should == 1
+      result[0][0].should == 'alice'
+      result[0][1].should == 'references'
+    end
+
+    it 'should return the set of data' do
+      @mock_fog_interface.stub(:get_execute_query).and_return(
+        {:Link => [
+          {:rel  => 'down',
+           :href => 'query?type=alice&#38;format=references'},
+          {:rel  => 'down',
+           :href => 'query?type=alice&#38;format=references'},
+        ]})
+
+      result = @query_runner.available_query_types
+
+      result.size.should == 2
+    end
   end
 
-  it 'return no results when fog results do not include a Record' do
-    @mock_fog_interface.stub(:get_execute_query).and_return(
+  context '#run' do
+    it 'should return no results when fog returns no results' do
+      @mock_fog_interface.stub(:get_execute_query).and_return({})
+
+      result = @query_runner.run()
+
+      result.should == []
+    end
+
+    it 'return no results when fog results do not include a "Record" or a "Reference"' do
+      @mock_fog_interface.stub(:get_execute_query).and_return(
         {
-            :WibbleBlob => {:field1 => 'Stuff 1'}
+          :WibbleBlob => {:field1 => 'Stuff 1'}
         }
-    )
+      )
 
-    result = @query_runner.run()
+      result = @query_runner.run()
 
-    result.size.should == 0 
-  end
+      result.size.should == 0
+    end
 
-  it 'should return a single result when fog returns a single result' do
-    fields = {:field1 => 'Stuff 1'}
-    @mock_fog_interface.stub(:get_execute_query).and_return(
+    it 'should return a single result when fog returns a single record' do
+      fields = {:field1 => 'Stuff 1'}
+      @mock_fog_interface.stub(:get_execute_query).and_return(
         {
-            :WibbleRecord => [fields]
+          :WibbleRecord => [fields]
         }
-    )
+      )
 
-    result = @query_runner.run()
+      result = @query_runner.run()
 
-    result.size.should == 1
-    result[0].should == fields
-  end
+      result.size.should == 1
+      result[0].should == fields
+    end
 
-  it 'should wrap single result from fog in list' do
-    fields = {:field1 => 'Stuff 1'}
-    @mock_fog_interface.stub(:get_execute_query).and_return(
+    it 'should return a single result when fog returns a single reference' do
+      fields = {:field1 => 'Stuff 1'}
+      @mock_fog_interface.stub(:get_execute_query).and_return(
         {
-            :WibbleRecord => fields
+          :WibbleReference => [fields]
         }
-    )
+      )
 
-    result = @query_runner.run()
+      result = @query_runner.run()
 
-    result.size.should == 1
-    result[0].should == fields
-  end
+      result.size.should == 1
+      result[0].should == fields
+    end
 
-  it 'should return all results returned by fog' do
-    fields = {:field1 => 'Stuff 1'}
-    more_fields = {:field1 => 'More Stuff 1'}
-    @mock_fog_interface.stub(:get_execute_query).and_return(
+    it 'should wrap single result from fog in list' do
+      fields = {:field1 => 'Stuff 1'}
+      @mock_fog_interface.stub(:get_execute_query).and_return(
         {
-            :WibbleRecord => [fields, more_fields]
+          :WibbleRecord => fields
         }
-    )
+      )
 
-    result = @query_runner.run()
+      result = @query_runner.run()
 
-    result.size.should == 2
-    result[0].should == fields
-    result[1].should == more_fields
+      result.size.should == 1
+      result[0].should == fields
+    end
+
+    it 'should return all results in a record returned by fog' do
+      fields      = {:field1 => 'Stuff 1'}
+      more_fields = {:field1 => 'More Stuff 1'}
+      @mock_fog_interface.stub(:get_execute_query).and_return(
+        {
+          :WibbleRecord => [fields, more_fields]
+        }
+      )
+
+      result = @query_runner.run()
+
+      result.size.should == 2
+      result[0].should == fields
+      result[1].should == more_fields
+    end
+
+    it 'should return the first item if more than one records provided' do
+      fields1 = {:field1 => 'Stuff 1'}
+      fields2 = {:field1 => 'Stuff 2'}
+      @mock_fog_interface.stub(:get_execute_query).and_return(
+        {
+          :WibbleRecord => [fields1],
+          :WobbleRecord => [fields2]
+        }
+      )
+
+      result = @query_runner.run()
+
+      result.size.should == 1
+      result[0].should == fields1
+    end
+
+    it 'should raise error if lastPage is not an integer' do
+      @mock_fog_interface.stub(:get_execute_query).and_return(
+        {
+          :lastPage     => :qwerty,
+          :WibbleRecord => []
+        }
+      )
+
+      expect { @query_runner.run() }.to raise_error('Invalid lastPage (qwerty) in query results')
+    end
+
+    it 'should get each page and collect the results' do
+      fields = {:field1 => 'Stuff 1'}
+      @mock_fog_interface.stub(:get_execute_query).and_return(
+        {
+          :lastPage     => 2,
+          :WibbleRecord => [fields]
+        }
+
+      )
+      result = @query_runner.run()
+
+      result.size.should == 2
+      result[0].should == fields
+      result[1].should == fields
+    end
   end
-
 end
 

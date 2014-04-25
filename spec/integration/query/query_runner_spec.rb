7459b1b4b228f49280344b9cfb968a06de2affaf
Mike Pountney <Mike.Pountney@gmail.com>
2014-04-25 15:31:28 +0100

    Add vApp based QueryRunner#run tests
    These involve setting up a known-good environment to query against,
    with two vApps.
    
    These tests replace the vAppTemplate tests, which assume too much. These
    will be removed in a subsequent commit, so not to confuse the additions
    in this commit.

--- spec/integration/query/query_runner_spec.rb
+++ spec/integration/query/query_runner_spec.rb
@@ -4,6 +4,30 @@ module Vcloud
   module Core
     describe QueryRunner do
 
+      required_env = {
+        'VCLOUD_VDC_NAME' =>
+           'to the name of an orgVdc to use to instanciate vApps into',
+        'VCLOUD_TEMPLATE_NAME' =>
+           'to the name of a vAppTemplate to use create vApps in tests',
+        'VCLOUD_CATALOG_NAME' =>
+           'to the name of the catalog that VCLOUD_VAPP_TEMPLATE_NAME is stored in',
+      }
+
+      error = false
+      required_env.each do |var,message|
+        unless ENV[var]
+          puts "Must set #{var} #{message}" unless ENV[var]
+          error = true
+        end
+      end
+      Kernel.exit(2) if error
+
+      before(:all) do
+        @vapp_template_name = ENV['VCLOUD_TEMPLATE_NAME']
+        @vapp_template_catalog_name = ENV['VCLOUD_CATALOG_NAME']
+        @vdc_name = ENV['VCLOUD_VDC_NAME']
+      end
+
       context "#available_query_types" do
 
         before(:all) do
@@ -65,6 +89,91 @@ module Vcloud
       end
 
       context "#run" do
+
+        before(:all) do
+          @number_of_vapps_to_create = 2
+          @test_case_vapps = create_test_case_vapps(
+            @number_of_vapps_to_create,
+            @vdc_name,
+            @vapp_template_catalog_name,
+            @vapp_template_name,
+          )
+        end
+
+        context "vApps are queriable with no options specified" do
+
+          before(:all) do
+            @all_vapps = QueryRunner.new.run('vApp')
+          end
+
+          it "returns an Array" do
+            expect(@all_vapps.class).to eq(Array)
+          end
+
+          it "returns at least the number of vApps that we created" do
+            expect(@all_vapps.size).to be > @number_of_vapps_to_create
+          end
+
+          it "returns a record with a defined :name field" do
+            expect(@all_vapps.first[:name]).not_to be_empty
+          end
+
+          it "returns a record with a defined :href field" do
+            expect(@all_vapps.first[:href]).not_to be_empty
+          end
+
+          it "returns a record with a defined :vdcName field" do
+            expect(@all_vapps.first[:vdcName]).not_to be_empty
+          end
+
+          it "returns a record with a defined :status field" do
+            expect(@all_vapps.first[:status]).not_to be_empty
+          end
+
+          it "does not return a 'bogusElement' element" do
+            expect(@all_vapps.first.key?(:bogusElement)).to be false
+          end
+
+        end
+
+        context "vApp query output fields can be limited to 'name,vdcName'" do
+
+          before(:all) do
+            @results = QueryRunner.new.run('vApp', fields: "name,vdcName")
+          end
+
+          it "returns a record with a defined href element" do
+            expect(@results.first[:href]).not_to be_empty
+          end
+
+          it "returns a record with a defined name element" do
+            expect(@results.first[:href]).not_to be_empty
+          end
+
+          it "returns a record with a defined vdcName element" do
+            expect(@results.first[:vdcName]).not_to be_empty
+          end
+
+          it "does not return a 'status' record" do
+            expect(@results.first.key?(:status)).to be false
+          end
+
+        end
+
+        context "query output can be restricted by a filter expression on name" do
+
+          before(:all) do
+            @vapp_name = @test_case_vapps.last.name
+            @filtered_results = QueryRunner.new.run('vApp', filter: "name==#{@vapp_name}")
+          end
+
+          it "returns a single record matching our filter on name" do
+            expect(@filtered_results.size).to be(1)
+            expect(@filtered_results.first.fetch(:name)).to eq(@vapp_name)
+          end
+
+        end
+
 
         context "when called with type vAppTemplate and no options" do
 
@@ -150,6 +259,30 @@ module Vcloud
             expect(filtered_result.first.fetch(:href)).to eq(@last_result_href)
           end
 
+        end
+
+        after(:all) do
+          fsi = Vcloud::Fog::ServiceInterface.new()
+          @test_case_vapps.each do |vapp|
+            fsi.delete_vapp(vapp.id)
+          end
+        end
+
+        def create_test_case_vapps(quantity, vdc_name, catalog_name, vapp_template_name)
+          vapp_template = VappTemplate.get(catalog_name, vapp_template_name)
+          timestamp_in_s = Time.new.to_i
+          base_vapp_name = "vcloud-core-query-tests-#{timestamp_in_s}-"
+          network_names = []
+          vapp_list = []
+          quantity.times do |index|
+            vapp_list << Vapp.instantiate(
+              base_vapp_name + index.to_s,
+              network_names,
+              vapp_template.id,
+              vdc_name
+            )
+          end
+          vapp_list
         end
 
       end

f062307dce8cb509155946a39b7ba92d68be00f1
Mike Pountney <Mike.Pountney@gmail.com>
2014-05-06 17:13:18 +0100

    Fix logic error in vApp#power_on test
    Whilst fixing up the expect syntax errors in 85e2639, this test
    started to fail.
    
    The test logic was wrong -- returning :status => 8 each
    time would emulate a machine NOT powering on.
    
    Updating the test to return 8 (STOPPED), then 4 (RUNNING)
    fixes the behaviour emulation, and hence the test now passes.

--- spec/vcloud/core/vapp_spec.rb
+++ spec/vcloud/core/vapp_spec.rb
@@ -136,7 +136,7 @@ module Vcloud
 
           it "should power on a vapp that is not powered on" do
             vapp = Vapp.new(@vapp_id)
-            @mock_fog_interface.should_receive(:get_vapp).twice().and_return({:status => 8})
+            @mock_fog_interface.should_receive(:get_vapp).twice().and_return({:status => 8}, {:status => 4})
             @mock_fog_interface.should_receive(:power_on_vapp).with(vapp.id)
             state = vapp.power_on
             expect(state).to be_true

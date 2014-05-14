8139a363582bb0e71f657867950b014fef155606
Mike Pountney <Mike.Pountney@gmail.com>
2014-05-14 17:13:39 +0100

    Add helpers to create/destroy vApp test fixtures
    We need to create and tear down vApps in many integration tests for
    Core -- Query, Vapp, Vm at least.
    
    The self.create_test_case_vapps method creates a number of vApps in
    a given vDC, with bridged networking to a list of networks. These
    can have a configurable name prefix, to help distinguish/separate them
    from other tests.
    
    The self.delete_vapps takes a list of Vapp objects as returned by
    self.create_test_case_vapps and destroys them.

---
+++ spec/support/integration_helper.rb
@@ -0,0 +1,32 @@
+module IntegrationHelper
+
+  def self.create_test_case_vapps(number_of_vapps,
+                                  vdc_name,
+                                  catalog_name,
+                                  vapp_template_name,
+                                  network_names = [],
+                                  prefix = "vcloud-core-tests-"
+                                 )
+    vapp_template = Vcloud::Core::VappTemplate.get(vapp_template_name, catalog_name)
+    timestamp_in_s = Time.new.to_i
+    base_vapp_name = "#{prefix}#{timestamp_in_s}-"
+    vapp_list = []
+    number_of_vapps.times do |index|
+      vapp_list << Vcloud::Core::Vapp.instantiate(
+        base_vapp_name + index.to_s,
+        network_names,
+        vapp_template.id,
+        vdc_name
+      )
+    end
+    vapp_list
+  end
+
+  def self.delete_vapps(vapp_list)
+    fsi = Vcloud::Fog::ServiceInterface.new()
+    vapp_list.each do |vapp|
+      fsi.delete_vapp(vapp.id)
+    end
+  end
+
+end

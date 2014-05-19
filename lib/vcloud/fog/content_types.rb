dd37d9e605caf7e217d84859e90873e5a324737b
Dan Carley <dan.carley@gmail.com>
2014-05-19 08:06:23 +0100

    Remove Fog::ContentTypes::CATALOG[_ITEM]
    These are no longer used by Vcloud::Core as of ba146c9. None of the other
    tools use these consts either.

--- lib/vcloud/fog/content_types.rb
+++ lib/vcloud/fog/content_types.rb
@@ -1,10 +1,8 @@
 module Vcloud
   module Fog
     module ContentTypes
-      CATALOG = 'application/vnd.vmware.vcloud.catalog+xml'
       ORG = 'application/vnd.vmware.vcloud.org+xml'
       VDC = 'application/vnd.vmware.vcloud.vdc+xml'
-      CATALOG_ITEM = 'application/vnd.vmware.vcloud.catalogItem+xml'
       NETWORK = 'application/vnd.vmware.vcloud.network+xml'
       METADATA = 'application/vnd.vmware.vcloud.metadata.value+xml'
     end

6e10ff5d068bfa29d2978f8ccee725c3b66c1dd7
Matt Bostock <matt@mattbostock.com>
2014-05-28 12:43:12 +0100

    Add integration tests for Core::EdgeGateway
    Add integration tests for the Vcloud::Core::EdgeGateway class:
    
      - EdgeGateway#get_by_name
      - EdgeGateway#get_ids_by_name
      - EdgeGateway.vcloud_attributes
      - EdgeGateway.name
      - EdgeGateway.href
      - EdgeGateway.interfaces
    
    The 'edge_gateway_id' parameter depends on version 0.6.0 of the vCloud
    Tools Tester gem.

--- spec/integration/edge_gateway/edge_gateway_spec.rb
+++ spec/integration/edge_gateway/edge_gateway_spec.rb
@@ -9,7 +9,9 @@ module Vcloud
         @test_data = Vcloud::Tools::Tester::TestParameters.new(config_file)
       end
 
-      let(:edge_gateway) { EdgeGateway.get_by_name(@test_data.edge_gateway) }
+      let(:edge_gateway)      { EdgeGateway.get_by_name(@test_data.edge_gateway) }
+      let(:edge_gateway_name) { @test_data.edge_gateway }
+      let(:edge_gateway_id)   { @test_data.edge_gateway_id }
 
       context "when updating the edge gateway" do
         before(:each) do
@@ -67,6 +69,61 @@ module Vcloud
         it "returns nil if network with given ID is not found" do
           network_interface = edge_gateway.vcloud_gateway_interface_by_id(SecureRandom.uuid)
           expect(network_interface).to be_nil
+        end
+      end
+
+      context "when retrieving an edge gateway ID (singular) by name" do
+        it "returns the an edge gateway object for that name" do
+          # See `let` statement above which calls EdgeGateway::get_by_name
+          expect(edge_gateway).to    be_a(Vcloud::Core::EdgeGateway)
+          expect(edge_gateway.id).to eq(edge_gateway_id)
+        end
+
+        it "raise an exception if edge gateway with given ID is not found" do
+          name = "this-would-never-exist"
+          expect { Vcloud::Core::EdgeGateway.get_by_name(name) }.to raise_error("edgeGateway #{name} not found")
+        end
+      end
+
+      context "when retrieving edge gateway IDs (plural) by name" do
+        it "returns the correct ID for that name" do
+          ids = Vcloud::Core::EdgeGateway.get_ids_by_name(edge_gateway_name)
+          expect(ids.first).to eq(edge_gateway_id)
+        end
+
+        it "returns an empty array if edge gateway with given ID is not found" do
+          name = "this-would-never-exist"
+          ids = Vcloud::Core::EdgeGateway.get_ids_by_name(name)
+          expect(ids).to eq([])
+        end
+      end
+
+      context "when retrieving vCloud attributes" do
+        it "returns the correct edge gateway for a given ID" do
+          vcloud_attributes = edge_gateway.vcloud_attributes
+          expect(vcloud_attributes[:href]).to include(edge_gateway_id)
+        end
+
+        it "returns the correct href for a given edge gateway" do
+          href = edge_gateway.href
+          expect(href).to include(edge_gateway_id)
+        end
+
+        it "returns the correct name for a given edge gateway" do
+          name = edge_gateway.name
+          expect(name).to eq(edge_gateway_name)
+        end
+      end
+
+      context "when retrieving a gateway's interfaces" do
+        let(:network_1) { @test_data.network_1 }
+
+        it "returns an array of interface objects" do
+          interfaces = edge_gateway.interfaces
+          network_1_interface = interfaces.detect { |i| i.name == network_1 }
+
+          expect(interfaces.first).to         be_a(Vcloud::Core::EdgeGatewayInterface)
+          expect(network_1_interface.name).to eq(network_1)
         end
       end
     end

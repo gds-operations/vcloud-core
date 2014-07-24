5f97d7a08ec8fe11cbbd53afb1e0c9b2203d0467
Sam J Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2014-07-24 11:01:07 +0100

    Export the RBENV_VERSION env var in build

--- jenkins_tests.sh
+++ jenkins_tests.sh
@@ -5,7 +5,7 @@ git clean -ffdx
 
 # We wish to force using a version of ruby installed with rbenv, because the version of ruby shipped with
 # Ubuntu 12.04 has a bug we hit in Psych when loading YAML files.
-RBENV_VERSION="1.9.3"
+export RBENV_VERSION="1.9.3"
 
 #FIXME: This can be removed once the tests have cycled through all 4 CI machines (a week or two after #100 is merged)
 # As we weren't using rbenv before, we've ended up with binstubs referencing ruby1.9.1

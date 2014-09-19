3c353e09612f2f0101e01b5d984acdd74e5e380b
Mike Pountney <Mike.Pountney@gmail.com>
2014-09-19 20:52:22 +0100

    Improve attach disk test; note ordering issue
    Improved the VM attach independent disk tests, to check that
    the disk was actually flagged as attached to the VM via the
    IndependentDisk#attached_vms call.
    
    In the process of this, discovered that the Vm#add_extra_disks method
    fails if there are independent disks already attached to the VM. I
    strongly suspect that this is due to the way the Fog Model handles
    adding local disks rather than it being a limitation of the vCD API itself,
    but fixing that is out of scope for this update.
    
    Added a test to document this behaviour and the exception raised by Fog.

--- spec/integration/core/vm_spec.rb
+++ spec/integration/core/vm_spec.rb
@@ -30,7 +30,7 @@ describe Vcloud::Core::Vm do
       @network_names,
       "vcloud-core-vm-tests"
     )
-    @test_case_independent_disks = IntegrationHelper.create_test_case_independent_disks(
+    @test_case_independent_disk_list = IntegrationHelper.create_test_case_independent_disks(
       1,
       @test_params.vdc_1_name,
       "100MB",
@@ -52,11 +52,6 @@ describe Vcloud::Core::Vm do
 
   end
 
-  context "#attach_independent_disks" do
-    it "can attach our fixture disk" do
-      expect(@vm.attach_independent_disks(@test_case_independent_disks)).to be_true
-    end
-  end
 
   context "#update_memory_size_in_mb" do
 
@@ -239,6 +234,31 @@ describe Vcloud::Core::Vm do
       expect(vm_nics[1][:IpAddress]).to eq(network_config[1][:ip_address])
       expect(vm_nics[1][:IpAddressAllocationMode]).to eq('MANUAL')
       expect(vm_nics[1][:NetworkConnectionIndex]).to eq('1')
+    end
+
+  end
+
+  context "#attach_independent_disks" do
+    it "can attach our fixture disk" do
+      disk = @test_case_independent_disk_list.first
+      expect(disk.attached_vms).to be_empty
+      @vm.attach_independent_disks(@test_case_independent_disk_list)
+      expect(disk.attached_vms.first.id).to eq(@vm.id)
+    end
+  end
+
+  # NB: It is suspected that this behaviour is caused by the Fog Model, not
+  #     vCloud Director itself. Issue raised on fog/fog to investigate/fix:
+  #     https://github.com/fog/fog/issues/3179
+  context "local disks cannot be added whilst an independent disk is attached" do
+
+    it "raises an error if we now try to add an extra local disk" do
+      extra_disks = [ { size: '10240' } ]
+      expect { @vm.add_extra_disks(extra_disks) }.
+        to raise_error(
+          Fog::Compute::VcloudDirector::BadRequest,
+          "The attached disks on VM \"#{@vm.name}\" cannot be modified."
+        )
     end
 
   end
